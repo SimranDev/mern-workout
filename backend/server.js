b26e29a777ff55db830fc895d538406be9812092
Simran <simran.develop@gmail.com>
2022-07-17 01:25:47 +1200

    express.json parsing + mongoose connection added

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,32 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
 const app = express();
 
 //middleware
+//express.json parses the incoming request with JSON payloads
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to the backend",
-  });
-});
+app.use("/api/workouts", workoutRoutes);
 
-//listening for requests
-app.listen(process.env.PORT, () => {
-  console.log(`*** Server is running on port ${process.env.PORT} ***`);
-});
+//connect to mongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`*** Server is running on port ${process.env.PORT} ***`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error:", err.message);
+  });
